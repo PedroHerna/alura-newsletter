39a2b5e431115b181c8d662e61e20778a32a3025
PedroHerna <pedro.hernandes1611@gmail.com>
2023-02-16 04:06:54 -0300

    Projeto criado

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend:{
+            colors:{
+                'azul':{
+                    'claro':'#C5DFFF',
+                    'escuro': '#061E3C',
+                    'hover': '#1057B0',
+                },
+            },
+            fontFamily:{
+                inter: ['Inter', 'sans-serif']
+            }
+        }
+    },
+  }
